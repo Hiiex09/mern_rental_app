ea98542936b74083b935e640929758810ebd9be0
barangandevon <barangandevon@gmail.com>
2026-04-17 21:44:14 +0800

    chore: added protected route middleware

---
+++ server/src/middleware/auth.middleware.js
@@ -0,0 +1,64 @@
+import jwt from "jsonwebtoken";
+import User from "../modules/auth/auth.model.js";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const access_token = req.cookies.access_token;
+    const refresh_token = req.cookies.refrsh_token;
+
+    if (!access_token) {
+      return res
+        .status(401)
+        .json({ message: "Unauthorize - No Token Provided" });
+    }
+
+    try {
+      const decoded = jwt.verify(access_token, process.env.ACCESS_TOKEN);
+      const user = await User.findById(decoded.userId).select("-password");
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      req.user = user;
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError" && refresh_token) {
+        try {
+          const decodedRefresh = jwt.verify(
+            refresh_token,
+            process.env.REFRESH_TOKEN,
+          );
+
+          const user = await User.findById(decodedRefresh._id).select(
+            "-password",
+          );
+
+          if (!user) {
+            return res.status(404).json({ message: "User not found" });
+          }
+
+          const newAccessToken = jwt.sign(
+            { userId: user._id },
+            process.env.ACCESS_TOKEN,
+            { expiresIn: "15mins" },
+          );
+
+          res.cookies("access_token", newAccessToken, {
+            maxAge: 15 * 60 * 1000,
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production",
+          });
+
+          req.user = user;
+          next();
+        } catch (error) {
+          return res.status(401).json({ message: "Invalid refresh token" });
+        }
+      }
+      return res.status(401).json({ message: "Invalid access token" });
+    }
+  } catch (error) {
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
