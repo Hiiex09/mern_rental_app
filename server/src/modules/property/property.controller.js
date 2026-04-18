9d978b0a239b9320b70f756020af6ac9a82dc168
barangandevon <barangandevon@gmail.com>
2026-04-18 13:29:25 +0800

    chore: fetching by id and update in property is now works

--- server/src/modules/property/property.controller.js
+++ server/src/modules/property/property.controller.js
@@ -1,5 +1,7 @@
 import { createPropertyServices } from "./services/createProperty.services.js";
 import { getAllPropertyServices } from "./services/getProperty.services.js";
+import { getPropertyServicesById } from "./services/getPropertyById.services.js";
+import { updatePropertyServices } from "./services/updatePropert.service.js";
 
 export const create_property = async (req, res) => {
   try {
@@ -42,5 +44,44 @@ export const get_all_properties = async (req, res) => {
     return res.status(500).json({
       message: "Internal Server Error",
     });
+  }
+};
+
+export const get_properties_by_id = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const fetchData = await getPropertyServicesById(id);
+
+    if (!fetchData) {
+      return res.status(404).json({ message: "Property not found" });
+    }
+
+    res.status(200).json({
+      message: "Data Fetched successfully",
+      fetchData,
+    });
+  } catch (error) {
+    console.error("Error in fetching data:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const update_property = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const update_data = await updatePropertyServices(id, req.body);
+
+    res.status(200).json({
+      message: "Update Successful",
+      update_data,
+    });
+  } catch (error) {
+    if (error.message === "Property Not Found") {
+      return res.status(404).json({ message: error.message });
+    }
+
+    console.error("Error in updating data:", error);
+    return res.status(500).json({ message: "Internal Server Error" });
   }
 };
