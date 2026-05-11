81da1836dec38253b9eeb534ce20430b036380c6
barangandevon <barangandevon@gmail.com>
2026-05-11 11:41:47 +0800

    chore: rewrite client

--- client/src/features/property/api.ts
+++
@@ -1,9 +0,0 @@
-import apiClient from "../../services/apiClient";
-
-export const apiProperties = {
-    getAll: async () => {
-        const response = await apiClient.get("/property/");
-        return response.data?.data ?? [];
-
-    },
-};
